678d7cf7c45fa232ae579c681c5624f1aa7cae1f
Benjamin J. Balter <Ben@Balter.com>
2012-06-28 00:50:25 -0300

    load class in example.

--- example.php
+++ example.php
@@ -6,9 +6,14 @@
 <html>
 	<body>
 <?php
- 
+
+//bootstrap the class
+require_once dirname( __FILE__ ) . 'class.federal-open-source-repos.php';
+
+//instantiate the class
 $gh = new Federal_Open_Source_Repos();
 
+//retrieve all repos and loop
 foreach ( $gh->get_repos() as $repo ) { ?>
 
 	<p><?php echo $repo->owner->login; ?>: <a href="<?php echo $repo->url; ?>"><?php echo $repo->name; ?></a><br />
